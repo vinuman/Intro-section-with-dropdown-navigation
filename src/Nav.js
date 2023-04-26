70757aa099ee71f065b7b6142d20a8ebb30f4228
Vinu Cyril <vinucyril@gmail.com>
2023-04-26 17:01:15 +0530

    feature list

--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,21 @@
 import logo from "./logo.svg";
 import arrowDown from "./icon-arrow-down.svg";
+import arrowUp from "./icon-arrow-up.svg";
+import todo from "./icon-todo.svg";
+import calendar from "./icon-calendar.svg";
+import reminders from "./icon-reminders.svg";
+import planning from "./icon-planning.svg";
+import { useState } from "react";
 
 const Nav = () => {
+  const [features, setFeatures] = useState(false);
+  const [company, setCompany] = useState(false);
+
+  const handleFeaturesClick = () => {
+    setFeatures(!features);
+    console.log(features);
+  };
+
   const style = {
     height: "2.6rem",
     width: "8.3rem",
@@ -11,15 +25,77 @@ const Nav = () => {
     paddingLeft: "0.6rem",
   };
 
+  const styleFeature = {
+    width: "15.3rem",
+    backgroundColor: "#fff",
+    borderRadius: "2rem",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    left: "10%",
+    top: "10%",
+    border: "1px solid #151515",
+    paddingBottom: "1rem",
+  };
+
+  const styleFeatureList = {
+    display: "flex",
+    justifyContent: "center",
+    width: "100%",
+  };
+
+  const styleIcon = {
+    width: "1.6rem",
+    height: "1.6rem",
+    marginTop: "1.8rem",
+  };
+
+  const styleList = {
+    color: "#686868",
+    fontSize: "1.4rem",
+    height: "100%",
+    paddingLeft: "1rem",
+  };
+
   return (
     <nav>
       <div className="left">
         <img style={style} src={logo}></img>
         <ul>
-          <li>
-            Features
-            <img style={style2} src={arrowDown}></img>
-          </li>
+          {features ? (
+            <div>
+              <li onClick={handleFeaturesClick}>
+                Features
+                <img style={style2} src={arrowUp}></img>
+              </li>
+              <div style={styleFeature} className="featureList">
+                <div style={styleFeatureList} className="feature-item">
+                  <img style={styleIcon} src={todo} alt="todo"></img>
+                  <p style={styleList}>To do list</p>
+                </div>
+                <div style={styleFeatureList} className="feature-item">
+                  <img style={styleIcon} src={calendar} alt="calendar"></img>
+                  <p style={styleList}>Calendar</p>
+                </div>
+                <div style={styleFeatureList} className="feature-item">
+                  <img style={styleIcon} src={reminders} alt="reminders"></img>
+                  <p style={styleList}>Reminders</p>
+                </div>
+                <div style={styleFeatureList} className="feature-item">
+                  <img style={styleIcon} src={planning} alt="planning"></img>
+                  <p style={styleList}>Planning</p>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <li onClick={handleFeaturesClick}>
+              Features
+              <img style={style2} src={arrowDown}></img>
+            </li>
+          )}
+
           <li>
             Company
             <img style={style2} src={arrowDown}></img>
